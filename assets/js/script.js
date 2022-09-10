6190a571246fa42a350ea3f20737386a1039cc57
John_considine <johnconsidine23@yahoo.co.uk>
2022-09-10 16:04:59 +0000

    add rules page and copy html content from html index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("connected")
+const menu = document.getElementById("menu");
+const hamburger = document.getElementById("hamburger-icon");
+
+hamburger.onclick = () => {
+    console.log("hello")
+    toggleMenu()
+}
+
+function toggleMenu() {
+    if (menu.style.display == "none") {
+        menu.style.display = "flex";
+    } else {
+        menu.style.display = "none"
+    }
+}
