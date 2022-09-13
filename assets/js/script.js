14a37de083e3b2ba6eed94f09cb95b213a56b06b
John_considine <johnconsidine23@yahoo.co.uk>
2022-09-13 16:33:39 +0000

    declare all variables from html source, add fucntions for each variable in javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,5 +16,50 @@ $(document).ready(function () {
         }
     }
 
+    const userScoretext = $("#userscore");
+    const botScoretext = $("#botscore");
+    const player = $("#player");
+    const bot = $("#bot");
+    const rock = $("#rock");
+    const paper = $("#paper");
+    const scissors = $("#scissors");
+    const lizard = $("#lizard");
+    const spock = $("#spock");
+
+    const options = {
+        ROCK: 0,
+        PAPER: 1,
+        SCISSORS: 2,
+        LIZARD: 3,
+        SPOCK: 4,
+    }
+
+    let userScore = 0;
+    let botScore = 0;
+
+    botScoretext.text("0");
+
+    rock.click(function () {
+        playBotTurn
+    })
+
+    paper.click(function () {
+        playBotTurn
+    })
+
+    scissors.click(function () {
+        playBotTurn
+    })
+
+    lizard.click(function () {
+        playBotTurn
+    })
+
+    spock.click(function () {
+        playBotTurn
+    })
+
+
+
 
 })
