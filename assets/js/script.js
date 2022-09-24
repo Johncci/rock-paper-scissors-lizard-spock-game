5af4366bfc6fd76667c4e557cd7241c36e5a6ce8
John_considine <johnconsidine23@yahoo.co.uk>
2022-09-24 15:20:45 +0000

    add content to readme including sreenshots for finished website, add styling to rules page to improve responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    //const declarations using jQuery
+
     const menu = $("#menu");
     const hamburger = $("#hamburger-icon");
     const userScoreText = $("#userscore");
@@ -11,9 +13,9 @@ $(document).ready(function () {
     const scissors = $("#scissors");
     const lizard = $("#lizard");
     const spock = $("#spock");
-    const outcome = $("#outcome")
+    const outcome = $("#outcome");
 
-
+    //assigning a numercial value to each selection option 
 
     const options = {
         ROCK: 0,
@@ -21,53 +23,64 @@ $(document).ready(function () {
         SCISSORS: 2,
         LIZARD: 3,
         SPOCK: 4,
-    }
+    };
+
+    // starts values at 0 and null that will change as the user plays the game
 
     let userScore = 0;
     let botScore = 0;
     let userChoice = null;
     let botChoice = null;
 
-
+    // function declaration for hamburger menu
 
     hamburger.click(function () {
-        toggleMenu()
-    })
+        toggleMenu();
+    });
+
+    /** The following are function declarations for picking any of the gameplay options as in rock, paper ,scissors, lizard and spock.
+     * The playUserTurn will select the clicked item for the user.
+     * The playBotTurn function will choose a random option.
+     * The makeDecision will then decide the winner of that particular round.
+     */
 
     rock.click(function () {
-        playUserTurn(options.ROCK)
-        playBotTurn()
-        makeDecision()
+        playUserTurn(options.ROCK);
+        playBotTurn();
+        makeDecision();
 
-    })
+    });
 
     paper.click(function () {
-        playUserTurn(options.PAPER)
-        playBotTurn()
-        makeDecision()
+        playUserTurn(options.PAPER);
+        playBotTurn();
+        makeDecision();
 
-    })
+    });
 
     scissors.click(function () {
-        playUserTurn(options.SCISSORS)
-        playBotTurn()
-        makeDecision()
+        playUserTurn(options.SCISSORS);
+        playBotTurn();
+        makeDecision();
 
-    })
+    });
 
     lizard.click(function () {
-        playUserTurn(options.LIZARD)
-        playBotTurn()
-        makeDecision()
+        playUserTurn(options.LIZARD);
+        playBotTurn();
+        makeDecision();
 
-    })
+    });
 
     spock.click(function () {
-        playUserTurn(options.SPOCK)
-        playBotTurn()
-        makeDecision()
+        playUserTurn(options.SPOCK);
+        playBotTurn();
+        makeDecision();
 
-    })
+    });
+
+    // The original method for the below hamburger function was created in vanilla javaScript.
+    // This was converted to jQuery css sourced from w3 schools.
 
     function toggleMenu() {
         if (menu.css("display") == "none") {
@@ -77,6 +90,11 @@ $(document).ready(function () {
         }
     }
 
+    /**The below method takes a random number from 0 to 4 and multiplies it by 5 and then math.floor rounds it down to a single number
+     * It then gets the option image for the bot.
+     * The bot.attr gets the random image and places it in the combatant box for the bot.
+     */
+
     function playBotTurn() {
         let random = Math.floor(Math.random() * 5);
         botChoice = random;
@@ -84,12 +102,16 @@ $(document).ready(function () {
         bot.attr("src", randomOptionImage);
     }
 
+    // The below function places the user choice selected option into the users combatant box by gettting the selected image. 
+
 
     function playUserTurn(selectedOption) {
         userChoice = selectedOption;
-        let selectedImage = getOptionImage(selectedOption)
-        player.attr("src", selectedImage)
+        let selectedImage = getOptionImage(selectedOption);
+        player.attr("src", selectedImage);
     }
+
+    // The below if/else statements decide all the possible outcomes for the game.
 
     function makeDecision() {
         if (userChoice == botChoice) {
@@ -148,15 +170,18 @@ $(document).ready(function () {
             }
         }
 
+        //These functions are called after the user selects a choice and when the game winning score is reached.
 
-        updateScore()
-        userWins()
-        botWins()
-        endGame()
+
+        updateScore();
+        userWins();
+        botWins();
+        endGame();
 
 
     }
 
+    //This function updates the score boxes.
 
     function updateScore() {
         botScoreText.text(botScore);
@@ -164,19 +189,25 @@ $(document).ready(function () {
 
     }
 
+    //This function updates the outcome of the game if the user wins and displays the assigned text.
+
 
     function userWins() {
         if (userScore === 5) {
-            outcome.text("Congratulations, You are the winner!")
+            outcome.text("Congratulations, You are the winner!");
         }
 
     }
+
+    //This function updates the outcome of the game if the bot wins and displays the assigned text.
 
     function botWins() {
         if (botScore === 5) {
-            outcome.text("Bot wins, better luck next time!")
+            outcome.text("Bot wins, better luck next time!");
         }
     }
+
+    //This function resets the game when the winning score is reached by the user or the bot.
 
     function endGame() {
         if (userScore == 5 || botScore == 5) {
@@ -185,6 +216,8 @@ $(document).ready(function () {
         }
 
     }
+
+    //This switch/case statement gets the images required for the above functions based on selections.
 
 
     function getOptionImage(option) {
@@ -205,19 +238,4 @@ $(document).ready(function () {
 
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+});
